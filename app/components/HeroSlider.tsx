f6c18fb3a2a24d451bc4d2e58b26b854f36246dd
KokiSato <kokingbeach@gmail.com>
2026-06-17 00:36:46 +0900

    Zoom animation 6s (linear): slides cut away mid-zoom

--- app/components/HeroSlider.tsx
+++ app/components/HeroSlider.tsx
@@ -63,7 +63,7 @@ export default function HeroSlider() {
             className="w-full h-full"
             style={
               i > 0
-                ? { animation: `heroZoom ${DURATIONS[i]}ms ease-in-out forwards` }
+                ? { animation: `heroZoom 6000ms linear forwards` }
                 : {}
             }
           >
